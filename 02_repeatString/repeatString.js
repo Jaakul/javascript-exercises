f3d01411e74fab0fdf188d538eba7768af18aa56
Jaakul <jackw1689@gmail.com>
2022-08-06 22:10:12 -0700

    repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+    let booty = "";
+    for (let i = 0; i < num; i++) {
+booty+=string;
 };
+if (num<0) {
+    return booty = 'ERROR'}
+if (num == ''){
+    return booty}
+
+if (num == 1) {
+    return string;
+}
+
+
+
+else {
+    return booty}
+
+}
+
 
 // Do not edit below this line
 module.exports = repeatString;
