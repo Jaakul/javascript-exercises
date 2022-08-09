925c76c7aa49c08ac14636b721c4cb20e4ecdc2c
Jaakul <jackw1689@gmail.com>
2022-08-09 10:42:37 -0700

    finished getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
 
-};
+  const getTheTitles = function(books) {
+return books.map(book => book.title);
 
+
+
+
+
+
+
+    /*   SOLUTION 1
+ let newArray = []
+    for (let i=0;i<books.length;i++){
+    newArray.push(books[i].title);
+    }
+    return newArray;
+    */
+}
 // Do not edit below this line
 module.exports = getTheTitles;
