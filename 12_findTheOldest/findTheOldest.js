4be4f7317d8fb50694b4b5a3665b4242ed2149e0
jacksmsg <jackw1689@gmail.com>
2022-08-09 15:59:55 -0700

    solved findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,15 +1,35 @@
-const findTheOldest = function(people) {
-people.reduce(
-    
-)
 
-//function to determine ages for every element
-    // create new array with name and age?
+const findTheOldest = function(people){
+    let oldest = people.reduce(reducer);
+    return oldest.name;
+}
 
 
-//reducer to compare all actual ages --> returns name 
+const todayDate = new Date();
+const year = todayDate.getFullYear();
 
-};
+function ageMaker(array){
+    if (array.yearOfDeath == undefined){
+return year - array.yearOfBirth
+    }
+    else {
+        return array.yearOfDeath - array.yearOfBirth
+    }
+}
+
+
+reducer = function (currentValue,nextValue) {
+return ageMaker(currentValue)>ageMaker(nextValue) ? currentValue : nextValue
+}
+
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
