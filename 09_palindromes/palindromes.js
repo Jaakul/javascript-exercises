dcfbf000dd8868526f3f7d7d4d5b011bfd613fb1
Jaakul <jackw1689@gmail.com>
2022-08-08 18:22:59 -0700

    palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+
+
+const palindromes = function (myString) {
+let arrFwd;
+let arrBckwd = [];
+let arrFwdFixed;
+let arrBckwdFixed;
+
+arrFwdUpper = myString.toUpperCase();
+const regex = /[a-zA-Z]/g
+arrFwd = arrFwdUpper.match(regex)
+arrFwdFixed = arrFwd.toString('');
+
+for (let i=arrFwd.length-1; i>=0;i--)
+arrBckwd.push(arrFwd[i]);
+arrBckwdFixed = arrBckwd.toString();
+
+
+if ( arrFwdFixed===arrBckwdFixed){
+    return true
+}
+else{
+    return false
+}
 
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
