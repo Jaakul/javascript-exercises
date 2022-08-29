21d033b9f4632c844d698597b7c311ac8d7dbc87
Jaakul <jackw1689@gmail.com>
2022-08-29 11:45:47 -0700

    dolne

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -36,3 +36,5 @@ module.exports = fibonacci;
 //1,1,2,3,5,8,13,21,34,55,89,144,
 //1,2,3,4,5,6,07,08,09,10,11,012,13,14,15,
 //0,1,2,3,4,5
+//redo this code.. you're thinking about this as if you need to publish 
+//the whole array, you do not!!! need to do that.
